2c9038eaca09d7e4dfcb60de24de62b623e6c371
willy384 <petersonmqt@gmail.com>
2025-06-16 02:08:49 -0300

    Add files via upload

---
+++ macro.py
@@ -0,0 +1,5 @@
+import pydirectinput
+import time
+import keyboard  # Biblioteca para detectar teclas pressionadas
+
+pydirectinput.press('space')  # Simula o pressionamento da tecla espaço
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#include <iostream>
+#include <chrono>
+#include <cstdlib> 
+int setPause(0);
+
+int main() {
+    //HWND hWnd = GetConsoleWindow();
+    //ShowWindow(hWnd, SW_HIDE); // Esconde o console
+    std::cout << "Aperte F1 para ativar e F2 para desativar!!" << std::endl;
+    while (true) {
+        if (GetAsyncKeyState(VK_F1) & 0x8000) {
+            while(true){
+                std::cout << "Ativado" << std::endl;
+                auto start = std::chrono::steady_clock::now();// Aguarda 50 segundos sem travar o programa
+                while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count() < 5) {
+                    if (GetAsyncKeyState(VK_F2) & 0x8000) {
+                        setPause=1;
+                        break;
+                    }
+                }
+                if(setPause==1){
+                    std::cout << "Desativado" << std::endl;
+                    break;
+                }
+                system("python macro.py"); // Executa o arquivo Python
+            }
+        }
+    }
+    return 0;
+}
+
+ 
